028a147c903945e70dc0fb48c2b7692b60ad30c4
narteysarso <narteysarso@gmail.com>
2021-05-14 05:43:22 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const PORT = process.env.PORT || 80;
+
+const server = http.createServer((req, res) => {
+    const body = [];
+
+    req.on('data', (data) => {
+        body.push(data);
+    });
+
+    req.on('end', () => {
+        const parsedData = Buffer.concat(body).toString();
+        console.log(parsedData);
+        res.write(parsedData);
+        res.end()
+    });
+    
+})
+
+
+server.listen(PORT, ()=> console.log(`server listening on port ${PORT}`))
