4468ed58765738f08a2405a11ac52b12be074fe5
narteysarso <narteysarso@gmail.com>
2021-06-04 20:53:46 +0000

    use environment port

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const app = require('express');
+const PORT = process.env.PORT || 3000;
 
 const server = app();
 
 
 server.use('/', (req, res) => {
     console.log(req.body);
-    
+
     res.json({"name": "data"});
 
 });
 
 
-
-server.listen(3000)
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
