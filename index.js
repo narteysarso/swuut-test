d37848c6c90ede661095077c62fb1b8ddecbe5b8
narteysarso <narteysarso@gmail.com>
2021-06-04 20:42:10 +0000

    using express

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-const http = require('http');
-const PORT = process.env.PORT || 5000;
+const app = require('express');
 
-const server = http.createServer((req, res) => {
-    const body = [];
-
-    req.on('data', (data) => {
-        body.push(data);
-    });
-
-    req.on('end', () => {
-        const parsedData = JSON.parse(Buffer.concat(body).toString());
-
-        let ok = " ";
-        
-
-        console.log(parsedData);
-
-        res.write(parsedData);
-        res.end()
-    });
-
-})
+const server = app();
 
 
-server.listen("5000", "0.0.0.0", () => console.log(`server listening on port ${PORT}`))
+server.use('/', (req, res) => {
+    console.log(req.body);
+    
+    res.json({"name": "data"});
+
+});
+
+
+
+server.listen(3000)
