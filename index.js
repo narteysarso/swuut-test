4e66fd2e2b3a40d574526c75c01f080071b1b6f2
narteysarso <narteysarso@gmail.com>
2021-06-04 16:11:25 +0000

    converted to use json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
     const body = [];
@@ -9,13 +9,84 @@ const server = http.createServer((req, res) => {
     });
 
     req.on('end', () => {
-        const parsedData = Buffer.concat(body).toString();
-        console.log(parsedData);
-        res.write(parsedData);
+        const parsedData = JSON.parse(Buffer.concat(body).toString());
+
+        let ok = " ";
+        try {
+            if (parsedData['action']) {
+                let getAction = parsedData['action'];
+                if (getAction == 'test') {
+                    ok = "{\"Status\":\"1\",\"Description\":\"Connection is ok\"}";
+                }
+                else if (getAction == 'auth') {
+                    if (parsedData[ 'terminal'] != '0') {
+                        ok = "{\"Status\":\"1\",\"Description\":\"Authorization is ok\"}";
+                    }
+                    else {
+                        ok = "{\"Status\":\"0\",\"Description\":\"Authorization is fail\"}";
+                    }
+                }
+                else if (getAction == 'query_rec') {
+                    if (parsedData[ 'start'] != '20200915') {
+                        ok = "{\"Status\":\"1\",\"List\":[\"202009180001\",\"202009180002\",\"202009180003\"]}";
+                    }
+                    else {
+                        ok = "{\"Status\":\"0\",\"Description\":\"Error!\"}";
+                    }
+                }
+                else if (getAction == 'query_detail') {
+                    if (parsedData[ 'title'] == '202009180001') {
+                        ok = "{\"Status\":\"1\",\"Description\":\"time:2020-09-18 12:30:30%%Terminal Number:1234%%Card Number:3556749171%%Ticket Value:10%%Card Balance:500%%Serial Number:0001\"}";
+                    }
+                    else if (parsedData[ 'title'] == '202009180002') {
+                        ok = "{\"Status\":\"1\",\"Description\":\"time:2020-09-18 20:12:15%%Terminal Number:1234%%Card Number:3556749171%%Ticket Value:5%%Card Balance:495%%Serial Number:0002\"}";
+                    }
+                    else {
+                        ok = "{\"Status\":\"0\",\"Description\":\"Error 2\"}";
+                    }
+                }
+                else if (getAction == 'query_sum') {
+                    ok = "{\"Status\":\"1\",\"Description\":\"Ticket count:100%%Ticket Amount Sum:5000\"}";
+                }
+                else if (getAction == 'query_tran') {
+                    if (parsedData[ 'serNum'] == '123456789') {
+                        ok = "{\"Status\":\"1\",\"Description\":\"time:2020-09-16 12:30:30%%Terminal Number:1234%%Card Number:3556749171%%Ticket Value:10%%Card Balance:500%%Serial Number:123456789\"}";
+                    }
+                    else {
+                        ok = "{\"Status\":\"0\",\"Description\":\"Error!\"}";
+                    }
+                }
+                else if (getAction == 'check') {
+                    ok = "{\"Status\":\"1\",\"Description\":\"Check connection is ok\"}";
+                }
+                else if (getAction == 'send_rec') {
+                    ok = "{\"Status\":\"1\",\"Description\":\"OK\"}";
+                }
+                else if (getAction == 'recharge') {
+                    ok = "{\"Status\":\"1\",\"Description\":\"OK\"}";
+                }
+                else if (getAction == 'issue') {
+                    ok = "{\"Status\":\"1\",\"Description\":\"OK\"}";
+                }
+                else {
+                    ok = "{\"Status\":\"0\",\"Description\":\"fail\"}";
+                }
+            }
+            else {
+                ok = "JSON Err";
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        
+
+        console.log(ok);
+
+        res.write(ok);
         res.end()
     });
-    
+
 })
 
 
-server.listen(PORT, ()=> console.log(`server listening on port ${PORT}`))
+server.listen("5000", "0.0.0.0", () => console.log(`server listening on port ${PORT}`))
